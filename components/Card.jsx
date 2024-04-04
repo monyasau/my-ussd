66eb983fadc7661166a21e43f2a698f149eb24f9
Olajide Olanrewaju <Olajide.m.olanrewaju@gmail.com>
2024-04-04 16:18:30 +0100

    Created a card component

---
+++ components/Card.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Button, View, Text } from "react-native";
+const Card=({cardData})=>{
+return( <>
+<View className=" bg-">
+
+    </View>
+</>)
+}
+export default Card;
