fd8bd719b387339a0057f5577e4242f04f7d2306
Olajide Olanrewaju <Olajide.m.olanrewaju@gmail.com>
2024-04-06 12:42:01 +0100

    disabled unused files

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,11 +1,11 @@
-import React from "react";
-import { Button, View, Text } from "react-native";
-const Card=({cardData})=>{
-return( <>
-<View className=" bg-gray-400 rounded-xl flex-4  border-white p-3 m-3">
-    <Text className="text-2xl">{cardData.title}</Text>
-    <Text>{cardData.content}</Text>
-    </View>
-</>)
-}
-export default Card;
+// import React from "react";
+// import { Button, View, Text } from "react-native";
+// const Card=({cardData})=>{
+// return( <>
+// <View className=" bg-gray-400 rounded-xl border-white p-4 py-6 m-4">
+//     <Text className="text-2xl">{cardData.title}</Text>
+//     <Text>{cardData.content}</Text>
+//     </View>
+// </>)
+// }
+// export default Card;
