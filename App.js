29cd72c1434f749c53f34b7d92d798ec6b84cb83
Olajide Olanrewaju <Olajide.m.olanrewaju@gmail.com>
2024-04-09 22:10:33 +0100

    removed unused  components

--- App.js
+++ App.js
@@ -10,10 +10,13 @@ import { Ionicons } from "@expo/vector-icons";
 import { useState } from "react";
 import BottomNavigation from "./components/BottomNavigation";
 import AboutModal from "./components/AboutModal";
+import { AirtimeCodes } from "./lib/ussdCodes";
+import ServicesGroup from "./components/ServicesGroup";
 
 export default function App() {
   const [aboutModalOpen, setAboutModalOpen] = useState(false);
   const switchAboutModal = () => setAboutModalOpen(!aboutModalOpen);
+
   const airtimeInfos = [
     { id: 1, name: "Check account balance", code: "310" },
     { id: 2, name: "Borrow airtime", code: "303" },
@@ -23,11 +26,11 @@ export default function App() {
     { id: 1, name: "Check data balance", code: "323" },
     { id: 2, name: "Share data", code: "321" },
     { id: 3, name: "Buy data", code: "312" },
-    { id: 4, name: "Special data bundle(MTN)", code: "121" },
-    { id: 8, name: "Night data plan(MTN)", code: "406*3" },
-    { id: 5, name: "Special data bundle(Glo)", code: "301" },
-    { id: 6, name: "Special data bundle(Airtel)", code: "121" },
-    { id: 7, name: "Special data bundle(9-Mobile)", code: "121" },
+    // { id: 4, name: "Special data bundle(MTN)", code: "121" },
+    // { id: 8, name: "Night data plan(MTN)", code: "406*3" },
+    // { id: 5, name: "Special data bundle(Glo)", code: "301" },
+    // { id: 6, name: "Special data bundle(Airtel)", code: "121" },
+    // { id: 7, name: "Special data bundle(9-Mobile)", code: "121" },
   ];
   const otherInfos = [
     { id: 1, name: "Customer care service", code: 300 },
@@ -38,51 +41,54 @@ export default function App() {
   return (
     <>
       <View className="h-full justify-between bg-white">
-        <View className="bg-green-600 p-5 pt-12">
-          <Text className="">TOP NAVIGATION</Text>
+        <View className="bg-primary/10 p-5 pt-12">
+          <Text className="text-sm text-primary">
+            <Text className="font-bold">MyUSSD</Text> - All your USSDs in one
+            place
+          </Text>
         </View>
+
+        <ScrollView className="py-5">
+          <ServicesGroup title="Airtime Services" data={AirtimeCodes} />
+          <ServicesGroup title="Data Services" data={AirtimeCodes} />
+
+          <View className="flex-grow py-5 bg-green-50">
+            <View
+              aria-label="Airtime services"
+              className="mx-5 bg-green-100 rounded-xl"
+            >
+              <Text className="mx-[10px] text-base">Airtime services</Text>
+              <MainApp groupData={airtimeInfos} />
+            </View>
+
+            <Line />
+            <View className="mx-5 bg-green-100" aria-label="Data services">
+              <Text className="text-base mx-[10px]">Internet/Data Service</Text>
+              <MainApp groupData={dataInfos} />
+            </View>
+            <Line />
+            <View
+              className="mx-5 bg-green-100 rounded-xl"
+              aria-label="Other services"
+            >
+              <Text className="mx-[10px] text-base">Other services</Text>
+              <MainApp groupData={otherInfos} />
+            </View>
+          </View>
+        </ScrollView>
+
         {aboutModalOpen && (
           <AboutModal
             active={aboutModalOpen}
             switchAboutModal={switchAboutModal}
           />
         )}
-
-        <View className="flex-grow py-5 bg-green-50">
-          <View
-            aria-label="Airtime services"
-            className="mx-5 bg-green-100 rounded-xl"
-          >
-            <Text className="mx-[10px] text-base">Airtime services</Text>
-            <MainApp groupData={airtimeInfos} />
-          </View>
-          <Line />
-          <View className="mx-5 bg-green-100" aria-label="Data services">
-            <Text className="text-base mx-[10px]">Internet/Data Service</Text>
-            <MainApp groupData={dataInfos} />
-          </View>
-          <Line />
-          <View
-            className="mx-5 bg-green-100 rounded-xl"
-            aria-label="Other services"
-          >
-            <Text className="mx-[10px] text-base">Other services</Text>
-            <MainApp groupData={otherInfos} />
-          </View>
-        </View>
-
         <BottomNavigation active={0} switchAboutModal={switchAboutModal} />
       </View>
       <StatusBar style="light" backgroundColor="#166534" />
     </>
   );
 }
-export function Line() {
-  return (
-    <View className="border-t border-green-400 my-5" aria-label="line"></View>
-  );
-}
-
 export function MainApp({ groupData }) {
   return (
     <ScrollView horizontal>
