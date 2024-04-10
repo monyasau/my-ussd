8ac18c886efe9e8d33cedb327ea8bf6cecba1127
Olajide Olanrewaju <Olajide.m.olanrewaju@gmail.com>
2024-04-10 06:58:07 +0100

    Adopted a new primary colour

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./App.{js,jsx,ts,tsx}","./components/**/*.{js,jsx,ts,tsx}"],
+  content: ["./App.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
 
   theme: {
     extend: {
-      colors:
-      {
-        primary: "#eee"
-      }
+      colors: {
+        primary: "#166534",
+      },
     },
   },
   plugins: [],
