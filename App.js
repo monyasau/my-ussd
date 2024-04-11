eec66a50ddfc9d256ac8b5fe3fbf0075e716b8e9
Olajide Olanrewaju <Olajide.m.olanrewaju@gmail.com>
2024-04-11 20:45:29 +0100

    refactoring: Removed some unused exports, and a few other changes

--- App.js
+++ App.js
@@ -3,19 +3,22 @@ import {
   Text,
   ScrollView,
   View,
-  TouchableOpacity,
-  Linking,
 } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
 import { useState } from "react";
 import BottomNavigation from "./components/BottomNavigation";
 import AboutModal from "./components/AboutModal";
 import { AirtimeCodes, DataCodes, OtherCodes } from "./lib/ussdCodes";
 import ServicesGroup from "./components/ServicesGroup";
+import RechargeModal from "./components/RechargeModal";
+import ShareModal from "./components/ShareModal";
 
 export default function App() {
   const [aboutModalOpen, setAboutModalOpen] = useState(false);
+  const [rechargeModalOpen, setRechargeModalOpen] = useState(false);
+  const [shareModalOpen, setShareModalOpen] = useState(false);
+  const switchShareModal = () => setShareModalOpen(!shareModalOpen);
   const switchAboutModal = () => setAboutModalOpen(!aboutModalOpen);
+  const switchRechargeModal = () => setRechargeModalOpen(!rechargeModalOpen);
 
   const otherInfos = [
     { id: 1, name: "Customer care service", code: 300 },
@@ -26,25 +29,38 @@ export default function App() {
   return (
     <>
       <View className="h-full justify-between bg-white">
-        <View className="bg-primary/10 p-5 pt-12">
+        <View className="bg-primary/10 p-5 pt-10">
           <Text className="text-sm text-primary">
-            <Text className="font-bold">MyUSSD</Text> - All your USSDs in one
+            <Text className="font-bold ">MyUSSD</Text> - All your USSDs in one
             place
           </Text>
         </View>
 
         <ScrollView className="py-5">
-          <ServicesGroup title="Airtime Services" data={AirtimeCodes} />
+          <ServicesGroup title="Airtime Services" data={AirtimeCodes} setRechargeModal={setRechargeModalOpen} setShareModal={setShareModalOpen} />
           <ServicesGroup title="Data Services" data={DataCodes} />
           <ServicesGroup title="Other Services" data={OtherCodes} />
         </ScrollView>
 
         {aboutModalOpen && (
           <AboutModal
-            active={aboutModalOpen}
+          active={aboutModalOpen}
             switchAboutModal={switchAboutModal}
           />
         )}
+        {rechargeModalOpen && (
+          <RechargeModal
+            switchRechargeModal={switchRechargeModal}
+          />
+        )}
+        {shareModalOpen && (
+          <ShareModal
+            switchShareModal={switchShareModal} action={shareModalAction}
+          />
+        )}
+
+
+
         <BottomNavigation active={0} switchAboutModal={switchAboutModal} />
       </View>
       <StatusBar style="light" backgroundColor="#166534" />
