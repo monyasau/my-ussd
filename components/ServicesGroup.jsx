58c7f447cf0b5b6e13030d794ee0a1572b7964c1
Olajide Olanrewaju <Olajide.m.olanrewaju@gmail.com>
2024-04-11 20:51:55 +0100

    Updated ui

--- components/ServicesGroup.jsx
+++ components/ServicesGroup.jsx
@@ -1,25 +1,42 @@
 import { Text, View, TouchableOpacity, Linking } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 
-export default function ServicesGroup({ title, data }) {
+export default function ServicesGroup({ title, data, setRechargeModal, setShareModal }) {
   return (
     <>
       <Text className="text-lg font-medium px-4">{title}</Text>
       <View className="flex-row flex-wrap py-4 px-2">
-        {data.map((item, i) => (
-          <TouchableOpacity
-            key={i}
-            className="w-1/2 p-2"
-            onPress={() =>
-              Linking.openURL(`tel:${encodeURIComponent(item.code)}`)
-            }
-          >
-            <View className="rounded-md p-4 bg-black/5">
-              <Ionicons name={item.icon} size={25} color="#166534" />
-              <Text className="text-xs pt-2">{item.title}</Text>
-            </View>
-          </TouchableOpacity>
-        ))}
+        {data.map((item, i) =>
+          !item.acceptInput ? (
+            <TouchableOpacity
+              key={i}
+              className="w-1/2 p-2"
+              onPress={() =>
+                Linking.openURL(`tel:${encodeURIComponent(item.code)}`)
+              }
+            >
+              <View className="rounded-md p-4 bg-black/5">
+                <Ionicons name={item.icon} size={25} color="#166534" />
+                <Text className="text-xs pt-2">{item.title}</Text>
+              </View>
+            </TouchableOpacity>
+          ) : (
+            <TouchableOpacity
+              key={i}
+              className="w-1/2 p-2"
+              onPress={() =>
+               {
+                setShareModal(true)
+               }
+              }
+            >
+              <View className="rounded-md p-4 bg-black/5">
+                <Ionicons name={item.icon} size={25} color="#166534" />
+                <Text className="text-xs pt-2">{item.title}</Text>
+              </View>
+            </TouchableOpacity>
+          )
+        )}
       </View>
     </>
   );
