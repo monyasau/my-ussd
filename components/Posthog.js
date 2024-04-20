866553db067b6c9f3956afd8f98320ec27c777d0
Olajide Olanrewaju <Olajide.m.olanrewaju@gmail.com>
2024-04-20 11:57:49 +0100

    update: update build info, updated analytics

--- components/Posthog.js
+++ components/Posthog.js
@@ -2,5 +2,7 @@
 import PostHog from 'posthog-react-native'
 
 export const posthog = new PostHog('phc_4YljC73RPgHC2OT8FUHJFQRzQL87TcsNyGjli4CxMzH', {
-host: 'https://app.posthog.com'
+host: 'https://app.posthog.com',
+flushAt: number = 8,  
+flushInterval: number = 10000,
 })
